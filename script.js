e894b033d0ea578d2448530b345595904127ba60
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-06-24 14:44:37 +0300

    sticky

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector(header);
 
 window.addEventListener("scroll", function() {
-    header.classlist.toggle("sticky", windows.scroll > 0);
+    header.classList.toggle("sticky", window.scrollY > 0 );
 });
